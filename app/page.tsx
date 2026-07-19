160f34bb9e257c79d20c80136ca11855e60f8d92
weerayosong <weerayosong@outlook.com>
2026-07-19 18:52:59 +0700

    chore: initial Next.js setup with Tailwind and clear default files

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <main className="flex min-h-screen items-center justify-center bg-neutral-950 text-white">
+            <h1 className="text-3xl font-bold text-amber-500">
+                โอม ศรี คณปติ ยะนะมะฮา
+            </h1>
+        </main>
+    );
+}
